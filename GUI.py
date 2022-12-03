0a176293c650fa3177c5e3fb3871803d2ee3ee70
Blueberry <augusto-vial@hotmail.com>
2022-12-02 21:30:53 -0300

    added help button

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, font
 from tkinter import filedialog
+from tkinter.ttk import Style
+
 from converter import *
 
 
@@ -20,6 +22,13 @@ def convertFile():
         messagebox.showerror('Erro', '"Nenhum arquivo selecionado / extensão inválida."')
 
 
+def helpButton():
+    messagebox.showinfo("Help", "Upon clicking the option: 'Convert File' and selecting your file, it will automatically be converted to either a PNG image or a GIF file; "
+                                "being save in the same place as the original file with _converted appended to it's name."
+                                "\n  "
+                                "\nThank you for using my little software!\n")
+
+
 class MainGui:
     def __init__(self, root):
 
@@ -30,10 +39,14 @@ class MainGui:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        ttk.Label(mainframe, text=" Universal Converter ", font="Cambria 16", borderwidth=2, relief="solid", anchor="center").grid(column=1, row=0, sticky=EW)
-        ttk.Button(mainframe, text="Convert to PNG", command=lambda: convertFile()).grid(column=1, row=1, sticky=EW)
-        #create progress bar
-        #create text sidebar explaning the software
+        s = Style()
+        s.configure('TButton', font=('Cambria', 12, "bold"))
+
+        ttk.Label(mainframe, text=" Universal Converter ", font="Cambria 16 bold", borderwidth=2, relief="solid", anchor="center").grid(column=1, row=0, sticky=EW)
+        ttk.Button(mainframe, text="Convert File", command=lambda: convertFile()).grid(column=1, row=1, sticky=EW)
+        ttk.Button(mainframe, text="?", style="TButton", width=2, command=lambda: helpButton()).grid(column=1, row=2, sticky=E)
+
+        # create progress bar
 
         # walks through all the widgets contained within our content frame and adds a bit of padding around each
         for child in mainframe.winfo_children():
